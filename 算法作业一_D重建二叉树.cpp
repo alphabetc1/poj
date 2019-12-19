1cce274b7c3bb0ce4ab8d2f9f8f2046a27d892c6
rhett <2508695655@qq.com>
2019-12-19 19:59:08 +0800

    A

--- 算法作业一_D重建二叉树.cpp
+++ 算法作业一_D重建二叉树.cpp
@@ -1,29 +1,30 @@
-#include <stdio.h>
+#include <iostream>
 #include <string>
-void findpath(int, int, int ,int);
-int n = 1, a[110], b[110];
-int main()
-{
+#include <string.h>
+#define NMAX 110
+using namespace std;
+
+int n = 1, a[NMAX], b[NMAX];
+
+void findPath(int starta, int enda, int startb, int endb) {
 	int i;
-	while (scanf("%d", &a[n]) != EOF && a[n] != -1)
-		n++;
-	n /= 2;
-	for (i = 1; i <= n ; i++)
-	{
-		b[i] = a[n + i];
-		a[n + i] = 0;
-	}
-	findpath(1, n, 1, n);
-	system("pause");
-	return 0;
+	if (starta > enda)
+		return;
+	cout << b[endb] << " ";
+	for (i = starta; i <= enda && a[i] != b[endb]; i++);
+	findPath(starta, i - 1, startb, startb + i - 1 - starta);
+	findPath(i + 1, enda, startb + i - starta, endb - 1);
 }
 
-void findpath(int starta, int enda, int startb, int endb)
-{
-	int i;
-	if (starta > enda)	return;
-	printf("%d ", b[endb]);
-	for (i = starta; i <= enda && a[i] != b[endb]; i++);
-	findpath(starta, i - 1, startb, startb + i - starta - 1);
-	findpath(i+1, enda, startb+i - starta, endb-1);
+int main() {
+	while (cin >> a[n]) {
+		n++;
+	}
+	n /= 2;
+	for (int i = 1; i <= n; i++) {
+		b[i] = a[n + i];
+	}
+	findPath(1, n, 1, n);
+	system("pause");
+	return 0;
 }
